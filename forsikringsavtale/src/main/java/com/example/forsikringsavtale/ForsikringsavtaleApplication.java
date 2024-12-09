04056965d98ef47c8892d863bd33fd29b5725a84
ivarkg <ivar.gjuvsland@fremtind.no>
2024-12-09 22:37:42 +0100

    SeOppCase: Setup java-project

---
+++ forsikringsavtale/src/main/java/com/example/forsikringsavtale/ForsikringsavtaleApplication.java
@@ -0,0 +1,13 @@
+package com.example.forsikringsavtale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForsikringsavtaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForsikringsavtaleApplication.class, args);
+	}
+
+}
---
+++ forsikringsavtale/src/test/java/com/example/forsikringsavtale/ForsikringsavtaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.forsikringsavtale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForsikringsavtaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
