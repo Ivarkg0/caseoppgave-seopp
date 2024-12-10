8c9f06ccfaf8e37ba9eac9c4703ad971925b7254
ivarkg <ivar.gjuvsland@fremtind.no>
2024-12-10 20:31:04 +0100

    Case seopp: New setup with working imports and project structure

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.forsikringsavtale;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ForsikringsavtaleApplicationTests {
+class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
